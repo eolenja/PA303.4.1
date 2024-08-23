666d71d0e1c09b0ee6bafac250b5f9983431680b
Elan Olenja <elan.olenja@Elans-Air.localdomain>
2024-08-23 15:12:12 -0400

    first commit

---
+++ src/Question4.java
@@ -0,0 +1,12 @@
+public class Question4 {
+    public static void main(String[] args) {
+        int x =5;
+
+        if (x > 10  || x < 20 )
+        {
+            System.out.println("out of range");}
+        else {
+            if (x <= 10 || x >= 20) {
+                System.out.println(" is in range");
+            }
+        }}}
---
+++ src/question3.java
@@ -0,0 +1,24 @@
+public class question3 {
+    public static void main(String[] args){
+        int x = 50;
+
+        if (x < 10  || x > 20 )
+        {
+            System.out.println("out of range");}
+                    else {
+                        if (x > 10 || x < 20 ) {
+                            System.out.println(  " is in range");
+
+
+                        }
+
+
+
+        }
+
+
+
+
+
+    }
+}
